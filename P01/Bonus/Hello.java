95b4983c746b0f13f667f28ee0dc73e5ebdf5f56
samridh20 <samridhsst@gmail.com>
2024-08-27 11:15:10 -0500

    push again

---
+++ P01/Bonus/Hello.java
@@ -0,0 +1,7 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("What is your name:");
+        Scanner obj = new Scanner(System.in)
+        System.out.println("Hello Samridh");
+    }
+}
