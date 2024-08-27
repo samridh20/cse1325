7cc97295a7140410913b0f5f470cd6cb3f95ce5a
samridh20 <samridhsst@gmail.com>
2024-08-27 11:24:32 -0500

    Update Hello.java

--- P01/Bonus/Hello.java
+++ P01/Bonus/Hello.java
@@ -1,7 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("What is your name:");
-        Scanner obj = new Scanner(System.in)
-        System.out.println("Hello Samridh");
+        Scanner name = new Scanner(System.in);
+
+
+        System.out.println("Hello " + name );
     }
 }
