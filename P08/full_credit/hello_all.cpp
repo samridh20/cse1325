764174b6fd74b59b569dcdec26ae4dc2b938308e
samridh20 <samridhsst@gmail.com>
2024-10-29 00:52:18 -0500

    updated hello_me and created hello_all

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+    std::cout <<" Hello Please enter your Name: " << '\n';
+    std:: string Name;
+    std::cin >> Name;
+    std::cout << "Hello, " << Name << '\n';
+    return 0;
+
+}
