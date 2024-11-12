ede7af371bb8ce2ce3cc2307fe62272a277b036c
samridh20 <samridhsst@gmail.com>
2024-11-11 21:42:10 -0600

    Bonus pre test

---
+++ P10/Bonus/Purse.h
@@ -0,0 +1,28 @@
+#ifndef PURSE_H
+#define PURSE_H
+
+#include <iostream>
+
+class Purse {
+public:
+    Purse(int pounds = 0, int shillings = 0, int pence = 0);
+    friend std::ostream& operator<<(std::ostream& os, const Purse& purse);
+    friend std::istream& operator>>(std::istream& is, Purse& purse);        // New input operator
+
+    auto operator<=>(const Purse&) const = default;
+
+    Purse& operator++();
+    Purse operator++(int);
+    Purse operator+(const Purse& other) const;
+    Purse operator-(const Purse& other) const;
+    Purse& operator+=(const Purse& other);
+    Purse& operator-=(const Purse& other);
+
+private:
+    int _pounds;
+    int _shillings;
+    int _pence;
+    void rationalize();
+};
+
+#endif 
---
+++ P10/Bonus/bank.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include "Purse.h"
+
+int main() {
+    std::cout << "Welcome to Ye Olde Bank of Merry England" << std::endl;
+
+    int accountCount;
+    std::cout << "How many accounts? ";
+    std::cin >> accountCount;
+
+    std::map<std::string, Purse> vault;
+
+    for (int i = 0; i < accountCount; ++i) {
+        std::cin.ignore();                                  // Ignore newline from previous input
+
+        std::string accountName;
+        std::cout << "Name account " << i << ": ";
+        std::getline(std::cin, accountName);
+
+        Purse purse;
+        std::cout << "Enter your initial deposit (# pounds shillingsd): ";
+        std::cin >> purse;
+
+        vault[accountName] = purse;
+        std::cout << "Account " << accountName << " created with " << vault[accountName] << std::endl;
+    }
+
+    std::cout << "\nAccount List" << std::endl;
+    std::cout << "============" << std::endl;
+
+    Purse total;
+    for (const auto& [name, purse] : vault) {
+        std::cout << "  " << name << " with " << purse << std::endl;
+        total += purse;
+    }
+
+    std::cout << "\nTotal in bank is " << total << std::endl;
+    return 0;
+}
